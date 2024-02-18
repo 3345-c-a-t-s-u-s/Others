322465640064b80c26f33e9a7897aff50e6c8918
CAT_SUS <117000269+3345-c-a-t-s-u-s@users.noreply.github.com>
2024-02-18 08:05:56 +0700

    Update Bee_Swarm_Simulator.lua

--- Bee_Swarm_Simulator.lua
+++ Bee_Swarm_Simulator.lua
@@ -1,8 +1,4 @@
-local v='https://raw.githubusercontent.com/3345-c-a-t-s-u-s/New-C4-Remote.lua/main/BeeSwarm.cpp';
-local g='HttpGet';
-local k = game;
-
-while true do task.wait()
-    k[g](k,v)
-  break
-end
+local v = game:HttpGet("https://raw.githubusercontent.com/3345-c-a-t-s-u-s/New-C4-Remote.lua/main/BeeSwarm.cpp")
+local func = loadstring(v)
+print('Old Loader')
+func()
